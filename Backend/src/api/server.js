4ced20d13b530882f07bdc91e8da0102ed916638
Rubia <rubiaemilly29@gmail.com>
2022-02-24 03:12:36 -0300

    Feat: Instala o JWT

--- Backend/src/api/server.js
+++ Backend/src/api/server.js
@@ -2,19 +2,12 @@ const express = require('express');
 require('dotenv-safe').load();
 const rescue = require('express-rescue');
 const cors = require('cors');
-// const fs = require('fs');
 const morgan = require('morgan');
-// const path = require('path');
 const errorRotas = require('../middleware/errorsRotas');
 const errorApi = require('../middleware/errorApi');
 const router = require('../router/router');
 
-// const accessLogStream = fs.createWriteStream(
-//     path.join('src', 'logs', 'access.log'), { flags: 'a' },
-// );
-
 const app = express();
-// app.use(morgan('combined', { stream: accessLogStream }));
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cors());
