19bd055d6da79fe35ca0f970d6e66fe2677bb549
Rubia <rubiaemilly29@gmail.com>
2022-02-23 22:57:21 -0300

    Fix: Alterando middleware de erros

--- Backend/src/middleware/errorApi.js
+++ Backend/src/middleware/errorApi.js
@@ -1,7 +1,8 @@
 function errorApi(err, _req, res, _next) {
-    console.log(err);
-   return res.status(err.status)
-    .send({ message: `Erro no inesperado, tente novamente mais tarde. (${err.data})` });
+    if (err.data) {
+        return res.status(err.response.status).send({ message: err.data }); 
+    }
+   return res.status(err.status).send({ message: err.message });
 }
 
 module.exports = errorApi;
